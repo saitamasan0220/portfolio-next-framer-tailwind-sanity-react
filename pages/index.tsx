24e4d774e8c8d3894da4c38dc7efe769883b2e58
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-10-03 03:39:37 -0400

    Projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
@@ -32,7 +33,9 @@ const Home: NextPage = () => {
         <Skills/>
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects/>
+      </section>
 
       {/* Contact Me */}
 
