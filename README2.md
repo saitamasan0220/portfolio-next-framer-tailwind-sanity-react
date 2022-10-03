8e5136932ba78924f023daa635ccc05ec8b50d30
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-10-03 14:31:46 -0400

    Projects section

--- README2.md
+++ README2.md
@@ -1,31 +1,47 @@
-# Visual Studio Code
+# General Setup
+
 ## VS Code Extensions
-* Tailwind CSS IntelliSense
-* ES7+ React/Redux/React-Native snippets
-* Auto Rename Tag
+
+-   Tailwind CSS IntelliSense
+-   ES7+ React/Redux/React-Native snippets
+-   Auto Rename Tag
+-   Prettier
+
+### Install Prettier
+
+CMD+Shift+P -> Settings UI
+Search for "Editor: Default Formatter", Choose Prettier
+Click the checkbox for "Editor: Format on Save"
 
 ## VS Code Commands
+
 CMD+P => Search bar for any file
 
 # VS Code Setup
+
 ## code . functionality in VS Code
+
 CMD Shift P => Shell Command: Install command in PATH => Press ENTER
 
 # Dependencies Setup
+
 ### nextjs.org/docs/basic-features/typescript
+
 ```
 npx create-next-app@latest --ts portfolio-next-framer-tailwind-sanity-react
 ```
 
 ## Tailwind CSS Setup
+
 ### tailwindcss.com/docs/guides/nextjs
+
 ```
 yarn add -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
 ```
 
-## Delete Home.module.css
-## styles > globals.css
+## Delete Home.module.css in styles > globals.css
+
 ```
 // Delete everything in there
 // add this
@@ -35,18 +51,33 @@ npx tailwindcss init -p
 @tailwind utilities;
 ```
 
-## React Social Icons
-```
-yarn add react-social-icons
-```
+### React Social Icons
 
-## Install Framer Motion
-## https://www.framer.com/docs/introduction/
-```
-yarn add framer-motion
-```
+`yarn add react-social-icons`
 
-## Other Dependencies
-```
-yarn add react-simple-typewriter
-```
+### Install Framer Motion
+
+#### https://www.framer.com/docs/introduction/
+
+`yarn add framer-motion`
+
+### Simple Typewriter
+
+`yarn add react-simple-typewriter`
+
+### Hero Icons
+
+#### heroicons.com
+
+`yarn add @heroicons/react`
+
+### React Hooks Form
+
+#### react-hook-form.com
+
+'yarn add react-hook-form'
+
+### Tailwind Scrollbar
+
+`yarn add -D tailwind-scrollbar`
+add `require("tailwind-scrollbar")` to `plugins` array in `tailwind.config.js`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,50 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import About from '../components/About'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
-import WorkExperience from '../components/WorkExperience'
+import type { NextPage } from "next";
+import Head from "next/head";
+import About from "../components/About";
+import ContactMe from "../components/ContactMe";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
-  return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen
-                    snap-y snap-mandatory overflow-scroll z-0'>
-      <Head>
-        <title>Daniel's Portfolio</title>
-      </Head>
+    return (
+        <div
+            className="bg-[rgb(36,36,36)] text-white h-screen
+                    snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80"
+        >
+            <Head>
+                <title>Daniel's Portfolio</title>
+            </Head>
 
-      <Header/>
+            <Header />
 
-      <section id="hero" className="snap-center">
-        <Hero/> 
-      </section>
-      
-      <section id="about" className='snap-center'>
-        <About/>
-      </section>
+            <section id="hero" className="snap-center">
+                <Hero />
+            </section>
 
-      <section id="experience" className='snap-center'>
-        <WorkExperience/>
-      </section>
+            <section id="about" className="snap-center">
+                <About />
+            </section>
 
-      <section id="skills" className="snap-start">
-        <Skills/>
-      </section>
+            <section id="experience" className="snap-center">
+                <WorkExperience />
+            </section>
 
-      <section id="projects" className="snap-start">
-        <Projects/>
-      </section>
+            <section id="skills" className="snap-start">
+                <Skills />
+            </section>
 
-      {/* Contact Me */}
+            <section id="projects" className="snap-start">
+                <Projects />
+            </section>
 
-    </div>
-  )
-}
+            <section id="contact" className="snap-start">
+                <ContactMe />
+            </section>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
