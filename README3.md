79478860844fa05a06775aaf2312870353af7d77
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-10-03 00:53:35 -0400

    Work Experience section

---
+++ README3.md
@@ -0,0 +1,5 @@
+# 20 VS Code Shortcuts For Pro Developers
+https://cult.honeypot.io/reads/20-vs-code-shortcuts-developers/#:~:text=Duplicate%20line,Mac%20%E2%80%94%20Command%20%2B%20Shift%20%2B%20D
+
+# Honeypot Jobs in Europe
+https://www.honeypot.io/?utm_source=cult&utm_medium=internal&utm_campaign=bottombanner&page=/contributors/viduni-wickramarachchi
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import WorkExperience from '../components/WorkExperience'
 
 const Home: NextPage = () => {
   return (
@@ -22,7 +23,9 @@ const Home: NextPage = () => {
         <About/>
       </section>
 
-      {/* Experience */}
+      <section id="experience" className='snap-center'>
+        <WorkExperience/>
+      </section>
 
       {/* Skills */}
 
