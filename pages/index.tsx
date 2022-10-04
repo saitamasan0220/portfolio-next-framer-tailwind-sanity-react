82fdbd84feba6ba6ff7885cb5561d15bb6dee0f3
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-10-04 06:46:09 -0400

    Added hero image button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
@@ -43,6 +44,18 @@ const Home: NextPage = () => {
             <section id="contact" className="snap-start">
                 <ContactMe />
             </section>
+
+            <Link href="#hero">
+                <footer className="sticky bottom-5 w-full cursor-pointer">
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer object-cover"
+                            src="https://media.comicbook.com/2018/12/one-punch-man-season-2-trailer-saitama-1151267.jpeg"
+                            alt=""
+                        />
+                    </div>
+                </footer>
+            </Link>
         </div>
     );
 };
